ad5ecde90ac4ed26f1db1b4496896301350326f8
jagannathpanigrahi313 <jagannathpanigrahi313@gmail.com>
2025-06-02 00:33:11 +0530

    Create primitive data tyoes .cpp

---
+++ primitive data tyoes .cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  int (variable) = 25;
+  cout << sizeof(variable)<< endl;//To see the size data tyoe in byte
+  cout << variable << emdl;
+  
+  return 0; 
+}
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  int (age) = 25;
+
+  cout << age << emdl;
+  
+  return 0; 
+}

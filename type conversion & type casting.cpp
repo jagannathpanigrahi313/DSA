532c8156949cc7f0fb14f73f8a275611ad562907
jagannathpanigrahi313 <jagannathpanigrahi313@gmail.com>
2025-06-17 15:20:11 +0530

    Update and rename type conversion & type casting to type conversion & type casting.cpp

---
+++ type conversion & type casting.cpp
@@ -0,0 +1,28 @@
+                        converting data type from from one type to another
+
+(1) type casting 
+    (implict)  this compiler does
+Small byte to big byte 
+
+EX 1: char to int
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  char (grade) = 'a';
+  int value = grade;// here char data type changed to int data type
+  cout << value  << emdl;
+  
+  return 0; 
+}
+
+(2) type casting 
+    (Explicit) this Programmer does 
+Big byte to small byte
+
+Ex 1 :
+
+  
+   

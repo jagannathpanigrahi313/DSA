85d3c3f6ca26a78ff7e6ae8d7310070a70513a8d
jagannathpanigrahi313 <jagannathpanigrahi313@gmail.com>
2025-06-21 18:35:44 +0530

    Update loops.cpp

--- loops.cpp
+++ loops.cpp
@@ -1,1 +1,34 @@
+note: count = count +1 OR count + =1 OR count++
 
+Q 1. print 1 to 10 
+  
+  #include <iostream>    |output : 1 2 3 4 5 6 7 8 9 10 
+using namespace std;     |
+                         |
+int main () {            |
+  int count = 1;         |
+
+  while (count <= 10){
+    cout << count <<" ";
+    count++;
+  }
+  cout <<endl;
+ return 0;
+}
+
+Q 2. print 1 to n 
+
+#include <iostream>     |output :1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 
+using namespace std;    |
+                        |
+int main () {           |  
+  int n = 50;           |
+  int count = 1;
+
+  while (count <= n){
+    cout << count <<" ";
+    count++;
+  }
+  cout <<endl;
+ return 0;
+}

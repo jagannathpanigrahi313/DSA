2087c05bf03758eb01c95fcb9416c8f004456c1c
jagannathpanigrahi313 <jagannathpanigrahi313@gmail.com>
2025-06-21 20:46:35 +0530

    Update 14. loops.cpp

--- 14. loops.cpp
+++ 14. loops.cpp
@@ -1,11 +1,11 @@
 note: count = count +1 OR count + = 1 OR count++
-      i =  i +2 OR i + = 2 OR i+2
+      i =  i +2 OR i + = 2 
   
 1 ) while loop
 
 Q 1. print 1 to 10 
   
-  #include <iostream>    |output : 1 2 3 4 5 6 7 8 9 10 
+  #include <iostream>    
 using namespace std;     
                          
 int main () {            
@@ -15,13 +15,13 @@ int main () {
     cout << count <<" ";
     count++;// if i not put this then output will infinite times 1
   }
-  cout <<endl;
+  cout <<endl;//output : 1 2 3 4 5 6 7 8 9 10 
  return 0;
 }
 
 Q 2. print 1 to n 
 
-#include <iostream>     |output :1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 
+#include <iostream>    |output :1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 
 using namespace std;    
                         
 int main () {           
@@ -38,21 +38,24 @@ int main () {
 }
 
 2 ) for loop
- #include <iostream>     |output : 1 3 5 7 9
-using namespace std;     |
-                         |
-int main () {            |
+ #include <iostream>     
+using namespace std;     
+                         
+int main () {            
   int n = 10;
 
-  for (int i = 1; i <= n;i+2){
-     count << i <<" ";
+  for (int i = 1; i <= n;i = i+2){
+     cout << i <<" ";//output : 1 3 5 7 9
   }
+      cout << endl;
+return 0;
+}
 
 Q 1) print sum of numbers from 1 to n
       
-#include <iostream>           |output : sum = 1275
-using namespace std;          |
-                              |
+#include <iostream>           
+using namespace std;          
+                        
 int main () {
   int n = 50;
   int sum  = 0;
@@ -61,7 +64,7 @@ int main () {
        sum += i;
  }
       
-  cout<<"sum = "<< sum << endl;
+  cout<<"sum = "<< sum << endl;//output : sum = 1275
  return 0;
 }
 

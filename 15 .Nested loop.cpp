fba485f8b9de4b1a4d9692c2fa10fed3da990a24
jagannathpanigrahi313 <jagannathpanigrahi313@gmail.com>
2025-06-22 12:39:19 +0530

    Update 15 .Nested loop.cpp

--- 15 .Nested loop.cpp
+++ 15 .Nested loop.cpp
@@ -2,4 +2,13 @@
 
 Q 1) to print 5 times  * in 5 line
 
-#include
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    for (int i=1; i<= 5; i++){
+        cout << "*****"<<endl;
+    }
+return 0;
+}

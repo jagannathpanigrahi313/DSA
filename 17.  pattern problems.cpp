e3a8c68b63142f1a45e0f5af319daf8272e0143d
jagannathpanigrahi313 <jagannathpanigrahi313@gmail.com>
2025-06-24 22:06:10 +0530

    Update 17.  pattern problems.cpp

--- 17.  pattern problems.cpp
+++ 17.  pattern problems.cpp
@@ -61,4 +61,24 @@ int main () {
    return 0;
 }
 -----------------------------------------------------------------------------------------------------
+                              continous square pattern 
+#include <iostream>
+using namespace std;
 
+int main () {
+   int n = 4;
+   
+   int num = 1;
+
+   for(int i=0; i<n; i++){
+       for(int j= 0; j< n; j++){
+           cout << num <<" ";
+           num++;
+       }
+       cout << endl;
+   }
+   
+   cout << "after pattern : "<< num << endl;//this is to verify your dry run thinking is correct or not
+
+   return 0;
+}

eaa3156fd7349ad4419e719aede94d571a93c45d
jagannathpanigrahi313 <jagannathpanigrahi313@gmail.com>
2025-06-26 19:08:51 +0530

    Create 20. inverted triangle pattern.cpp

---
+++ 20. inverted triangle pattern.cpp
@@ -0,0 +1,49 @@
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    int n = 4;
+
+    int num = 1;
+    for(int i = 0; i < n; i++) { 
+        //spaces 
+        for(int j = 0; j < i; j++) {
+            cout << " ";
+        }
+
+        //nums
+        for( int j = 0;j< n-i;j++){
+            cout << i+1 ;//output:1111
+                          //       222
+                          //        33
+                          //         4
+        }
+        cout << endl;              // Move to next line
+    }
+
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    char ch = 'B'; // starting character
+
+    for (int i = 0; i < 3; i++) { // 3 lines
+        // Print spaces
+        for (int j = 0; j < i; j++) {
+            cout << " ";
+        }
+
+        // Print characters (3 - i times)
+        for (int j = 0; j < 3 - i; j++) {
+            cout << char(ch + i);//OUTPUT :BBB
+        }                       //          CC
+                               //            D
+        cout << endl;
+    }
+
+    return 0;
+}
